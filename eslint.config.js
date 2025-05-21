d256c8521a8d6a34d760788c1b1917f397923d6c
Sebastian H <shnandezp@proton.me>
2025-05-20 20:03:41 -0400

    Init expo

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
+
+export default defineConfig([
+  expoConfig,
+  {
+    ignores: ["dist/*"],
+  },
+]);
