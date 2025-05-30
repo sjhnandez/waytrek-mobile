372d53556cfe4182f60d3ac8e42e27c305326b86
Sebastian H <shnandezp@proton.me>
2025-05-30 15:58:36 -0400

    refactor: move splash screen logic to layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,26 @@
 import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import Animated, { FadeOut } from "react-native-reanimated";
+import Splash from "../components/pages/splash";
 
 const RootLayout = () => {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  const [splashVisible, setSplashVisible] = useState(true);
+
+  useEffect(() => {
+    const visibleTimer = setTimeout(() => setSplashVisible(false), 3000);
+
+    return () => {
+      clearTimeout(visibleTimer);
+    };
+  });
+
+  return splashVisible ? (
+    <Animated.View exiting={FadeOut} style={{ flex: 1 }}>
+      <Splash />
+    </Animated.View>
+  ) : (
+    <Stack screenOptions={{ headerShown: false }} />
+  );
 };
 
 export default RootLayout;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,26 +1,7 @@
-import { useEffect, useState } from "react";
 import { View } from "react-native";
-import Animated, { FadeOut } from "react-native-reanimated";
-import Splash from "../components/pages/splash";
 
 const Index = () => {
-  const [splashVisible, setSplashVisible] = useState(true);
-
-  useEffect(() => {
-    const visibleTimer = setTimeout(() => setSplashVisible(false), 3000);
-
-    return () => {
-      clearTimeout(visibleTimer);
-    };
-  });
-
-  return splashVisible ? (
-    <Animated.View exiting={FadeOut} style={{ flex: 1 }}>
-      <Splash />
-    </Animated.View>
-  ) : (
-    <View>Home</View>
-  );
+  return <View>Home</View>;
 };
 
 export default Index;
