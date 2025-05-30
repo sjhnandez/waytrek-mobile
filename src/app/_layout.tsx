87cee971fa7bba3f6cfc27902e0592e8bd040445
Sebastian H <shnandezp@proton.me>
2025-05-30 16:14:52 -0400

    Revert "refactor: move splash screen logic to layout"
    This reverts commit 372d53556cfe4182f60d3ac8e42e27c305326b86.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,26 +1,7 @@
 import { Stack } from "expo-router";
-import { useEffect, useState } from "react";
-import Animated, { FadeOut } from "react-native-reanimated";
-import Splash from "../components/pages/splash";
 
 const RootLayout = () => {
-  const [splashVisible, setSplashVisible] = useState(true);
-
-  useEffect(() => {
-    const visibleTimer = setTimeout(() => setSplashVisible(false), 3000);
-
-    return () => {
-      clearTimeout(visibleTimer);
-    };
-  });
-
-  return splashVisible ? (
-    <Animated.View exiting={FadeOut} style={{ flex: 1 }}>
-      <Splash />
-    </Animated.View>
-  ) : (
-    <Stack screenOptions={{ headerShown: false }} />
-  );
+  return <Stack screenOptions={{ headerShown: false }} />;
 };
 
 export default RootLayout;
