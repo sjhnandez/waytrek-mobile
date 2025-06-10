0f34c1812843f39c58f64adf086c77ad8743d397
Sebastian H <shnandezp@proton.me>
2025-06-10 11:39:56 -0400

    fix: add missing dependency array, update splash duration

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,16 +3,21 @@ import { View } from "react-native";
 import Animated, { FadeOut } from "react-native-reanimated";
 import Splash from "../components/pages/splash";
 
+const splashDuration = 2;
+
 const Index = () => {
   const [splashVisible, setSplashVisible] = useState(true);
 
   useEffect(() => {
-    const visibleTimer = setTimeout(() => setSplashVisible(false), 3000);
+    const visibleTimer = setTimeout(
+      () => setSplashVisible(false),
+      1000 * splashDuration
+    );
 
     return () => {
       clearTimeout(visibleTimer);
     };
-  });
+  }, []);
 
   return splashVisible ? (
     <Animated.View exiting={FadeOut} style={{ flex: 1 }}>
